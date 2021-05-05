3b2ad9aecd988b4669805e68cf42e36a9ab7168c
Youngseok Bae <young.seok@hotmail.com>
2021-05-05 21:02:00 +0000

    Pair programming wiht Kevin: wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
+     for (l of horizontalJoin) {
+         if (l.includes(word)) {
+             return true;
+             continue;
+         }
+     }
+   let newArr = [];
+   for (let a = 0; a < letters[0].length; a++) {
+     let newArr2 = [];
+     for (let b = 0; b < letters.length; b++) {
+       newArr2.push(letters[b][a])
+     }
+     newArr.push(newArr2);
+     
+   }
+   const verticalJoin = newArr.map((val) => val.join(''));
+   for (a of verticalJoin) {
+     if (a.includes(word)) {
+       return true;
+       continue;
+     }
+   }
+   return false;
+ }
+ 
+ 
+ 
 module.exports = wordSearch
